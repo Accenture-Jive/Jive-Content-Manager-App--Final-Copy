055cdbc661050c7f2e542f5f69e6c48dd5bf9bb9
Ripul Thakur <ripul.thakur@accenture.com>
2013-02-14 20:06:54 +0530

    commit changes

--- js/movendelete.js
+++ js/movendelete.js
@@ -200,7 +200,7 @@ getContent(templateSpace);
 function getContent(source) {
 osapi.jive.corev3.contents.get({
 fields: '@all',
-count  :50,
+count  :1000,
 uri: source
 }).execute(onContentFetch);
 }
