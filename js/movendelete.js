90d3b5e7fab5d077b21d1be29d91c779f88b0eee
Ripul Thakur <ripul.thakur@accenture.com>
2013-02-14 23:25:35 +0530

    commit changes

--- js/movendelete.js
+++ js/movendelete.js
@@ -219,7 +219,7 @@ function getContent(source,target_groupurl,contentType) {
 if(CONTENT_TYPE_BLOG == contentType){
 	osapi.jive.corev3.contents.get({
 	fields: '@all',
-	count  :50,
+	count  :1000,
 	uri: source
 	}).execute(onContentFetchForBlog);
 	}
@@ -227,7 +227,7 @@ if(CONTENT_TYPE_BLOG == contentType){
 	
 	osapi.jive.corev3.contents.get({
 	fields: '@all',
-	count  :50,
+	count  :1000,
 	uri: source
 	}).execute(onContentFetch);
 	
