4713f8bb765d73bd37ebd747ee8d4398dc0f7a24
Ripul Thakur <ripul.thakur@accenture.com>
2013-02-18 16:48:08 +0530

    commit changes

--- js/movendelete.js
+++ js/movendelete.js
@@ -308,11 +308,12 @@ response.destroy().execute(updateResponse);
 
 function updateResponse(response) {
 if (response.error) {
-console.log("jsonError "+JSON.stringify(response));
+console.log("JSON Error: "+JSON.stringify(response));
 noOfFileFailed = noOfFileFailed + 1;
 return;
 }
 
+console.log("JSOn Success: "+JSON.stringify(response));
 noOfFileExecuted = noOfFileExecuted + 1;
 console.log("noOfFileExecuted: "+noOfFileExecuted);
 console.log("noOfFileFailed: "+noOfFileFailed);
