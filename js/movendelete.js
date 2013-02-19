c3d2e33f85f6112b2b341b38ba965ef92038cf3d
Ripul Thakur <ripul.thakur@accenture.com>
2013-02-19 15:18:23 +0530

    commit changes

--- js/movendelete.js
+++ js/movendelete.js
@@ -78,8 +78,8 @@ var templateSpace='';
 $("#start_copying_button").hide();
 $("#change_contents").hide();
 $("#button_div").hide();
-var iframePage = '<iframe id="frame1" src = "" style="width:650px;height:90px;margin-top:0px;font-family:Tahoma"></iframe>';
-document.getElementById("change_selection_div").innerHTML=iframePage;  
+var iframePage = '<iframe id="frame1" src = "" style="position:absolute;width:650px;height:90px;margin-top:0px;font-family:Tahoma"></iframe>';
+document.getElementById("selected_items").innerHTML=iframePage;  
 $("#copyTo").text("Moving this:");
 
 var str='';
